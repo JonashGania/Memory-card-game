4d97df1ca9489d0e4950952513deff3bddeb1af9
Jonash Gania <jonashtiglao@gmail.com>
2023-11-11 23:03:46 +0800

    import FetchPokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
+import { useState } from 'react'
 import './styles/App.css'
+import FetchPokemon from './fetchPokemon'
 
 function App() {
 
   return (
-    <>
-    </>
+    <div>
+      <FetchPokemon />
+    </div>
   )
 }
 
