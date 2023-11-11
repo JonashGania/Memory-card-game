89f0500253f30b2600c4737b43b032bb4201877c
Jonash Gania <jonashtiglao@gmail.com>
2023-11-11 23:04:21 +0800

    added pokemon cards render

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,30 @@
+import '../styles/card.css'
 
-export default function Card(){
+export default function Card({ pokemons }){
+
     return (
         <div>
-            
+            <div className="card-wrapper">
+                <ul>
+                    {pokemons.map((card, index) => (
+                        <li key={index}>
+                            <div className="card-container">
+                                <div className="front">
+                                    <img 
+                                        src={card.image} 
+                                        alt="pokemon image" 
+                                        className="card-image"
+                                    />
+                                    <h3 className="pokemon-name">{card.name}</h3>
+                                </div>
+                                <div className="back">
+
+                                </div>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
