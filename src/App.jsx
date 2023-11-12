ef3028ab144fb82d223f7ba675fa79f5fe6c0e0c
Jonash Gania <jonashtiglao@gmail.com>
2023-11-12 16:50:25 +0800

    import Score component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './styles/App.css'
 import FetchPokemon from './fetchPokemon'
+import Score from './components/Score'
 
 function App() {
 
   return (
-    <div>
+    <div className='app'>
+      <Score />
       <FetchPokemon />
     </div>
   )
