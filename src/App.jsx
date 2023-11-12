9079f3133dbae2004ecf1fcc4bb7060b93346744
Jonash Gania <jonashtiglao@gmail.com>
2023-11-12 22:41:34 +0800

    added current score & best score state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './styles/App.css'
-import FetchPokemon from './fetchPokemon'
+import useFetchPokemon from './fetchPokemon'
 import Score from './components/Score'
+import Card from './components/Card'
 
 function App() {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const pokemonData = useFetchPokemon();
+
+
+  const handleCardClick = (id) => {
+    let newSelectedCards;
+
+    if(selectedCards.includes(id)){
+      if(currentScore > bestScore){
+        setBestScore(currentScore);
+      }
+
+      setCurrentScore(0);
+      newSelectedCards = [];
+
+    } else {
+      setCurrentScore(currentScore + 1);
+      newSelectedCards = [...selectedCards, id];
+    }
+
+    if(newSelectedCards.length === pokemonData.length){
+      setCurrentScore(0);
+      setBestScore(10);
+      newSelectedCards = [];
+    }
+
+    setSelectedCards(newSelectedCards);
+  }
+
 
   return (
     <div className='app'>
-      <Score />
-      <FetchPokemon />
+      <Score currentScore={currentScore} bestScore={bestScore}/>
+      <Card pokemons={pokemonData} onCardClick={handleCardClick}/>
     </div>
   )
 }
