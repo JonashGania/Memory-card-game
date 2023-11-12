ee57e3b77cf1c3b69b4083e592eb2efd39d7d2b7
Jonash Gania <jonashtiglao@gmail.com>
2023-11-12 16:52:26 +0800

    added score board

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import '../styles/score.css'
 
-export default function Score() {
+export default function Score( {currentScore, bestScore} ) {
   return (
-    <div>Score</div>
+    <div className='score-container'>
+      <div className="game-title">
+        <img 
+          src="/pokeball.png" 
+          alt="pokeball"
+        />
+        <h2 className='game-name'>Poke<span>Cards</span></h2>
+      </div>
+      <div className="score-board">
+        <p className='currentScore'>
+          Score: <b>{currentScore}</b>
+        </p>
+        <p>/</p>
+        <p>
+          High Score: <b>{bestScore}</b>
+        </p>
+      </div>
+    </div>
   )
 }
