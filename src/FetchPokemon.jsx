fc17e920c8e6d3943fc0f696aeaa1692472f0da8
Jonash Gania <jonashtiglao@gmail.com>
2023-11-12 22:42:38 +0800

    changed function to custom hook

--- src/FetchPokemon.jsx
+++ src/FetchPokemon.jsx
@@ -1,47 +1,86 @@
-import React,{ useEffect, useState } from 'react';
-import Card from './components/Card';
+import { useEffect, useState } from 'react';
 
-export default function FetchPokemon() {
-    const [pokemonNames, setPokemonNames] = useState([]);
+export default function useFetchPokemon() {
+  const [pokemonNames, setPokemonNames] = useState([]);
 
-    const getRandomPokemonId = () => {
-        return Math.floor(Math.random() * 898) * 1;
+  const getRandomPokemonId = () => {
+    return Math.floor(Math.random() * 898) + 1;
+  };
+
+  const fetchPokemon = async (id) => {
+    try {
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+      const data = await response.json();
+      const name = data.name;
+      const image = data.sprites.front_default;
+      return { id, name, image };
+    } catch (error) {
+      console.error('Error fetching Pokémon:', error.message);
+    }
+  };
+
+  const getRandomPokemons = async () => {
+    const randomPokemonNames = [];
+    for (let i = 0; i < 10; i++) {
+      const randomId = getRandomPokemonId();
+      const pokemonName = await fetchPokemon(randomId);
+      randomPokemonNames.push(pokemonName);
     }
 
-    const fetchPokemon = async(id) => {
-        try{
-            const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
-            if(!response.ok){
-            throw new Error('Network response was not ok.');
-            }
-            const data = await response.json();
-            const name = data.name;
-            const image = data.sprites.front_default;
-            return { name, image };
-        } catch (error) {
-            console.error('Error fetching Pokémon:', error.message);
-        }
-    }
+    setPokemonNames(randomPokemonNames);
+  };
 
-    const getRandomPokemons = async () => {
-        const randomPokemonNames = [];
-        for(let i = 0; i < 10; i++){
-        const randomId = getRandomPokemonId();
-        const pokemonName = await fetchPokemon(randomId);
-        randomPokemonNames.push(pokemonName);
-        }
+  useEffect(() => {
+    getRandomPokemons();
+  }, []);
 
-        setPokemonNames(randomPokemonNames);
-    }
-
-    useEffect(() => {
-        getRandomPokemons();
-    }, []);
-  
-
-    return (
-        <div>
-            <Card pokemons={pokemonNames} />
-        </div>
-    )
+  return pokemonNames;
 }
+
+// import React,{ useEffect, useState } from 'react';
+// import Card from './components/Card';
+
+// export default function FetchPokemon() {
+//     const [pokemonNames, setPokemonNames] = useState([]);
+
+//     const getRandomPokemonId = () => {
+//         return Math.floor(Math.random() * 898) * 1;
+//     }
+
+//     const fetchPokemon = async(id) => {
+//         try{
+//             const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+//             if(!response.ok){
+//             throw new Error('Network response was not ok.');
+//             }
+//             const data = await response.json();
+//             const name = data.name;
+//             const image = data.sprites.front_default;
+//             return { id, name, image };
+//         } catch (error) {
+//             console.error('Error fetching Pokémon:', error.message);
+//         }
+//     }
+
+//     const getRandomPokemons = async () => {
+//         const randomPokemonNames = [];
+//         for(let i = 0; i < 10; i++){
+//         const randomId = getRandomPokemonId();
+//         const pokemonName = await fetchPokemon(randomId);
+//         randomPokemonNames.push(pokemonName);
+//         }
+
+//         setPokemonNames(randomPokemonNames);
+//     }
+
+//     useEffect(() => {
+//         getRandomPokemons();
+//     }, []);
+  
+//     return pokemonNames;
+
+//     // return <Card pokemons={pokemonNames}/>
+// }
