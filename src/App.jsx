6912a8df4a4cadbfd13dad6e19afbf99532bd01a
Jonash Gania <jonashtiglao@gmail.com>
2023-11-13 23:40:08 +0800

    added game over state

--- src/App.jsx
+++ src/App.jsx
@@ -3,46 +3,70 @@ import './styles/App.css'
 import useFetchPokemon from './fetchPokemon'
 import Score from './components/Score'
 import Card from './components/Card'
+import GameOver from './components/GameOver'
+import LoadingScreen from './components/LoadingScreen'
 
 function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [bestScore, setBestScore] = useState(0);
   const [selectedCards, setSelectedCards] = useState([]);
-  const pokemonData = useFetchPokemon();
-
+  const [gameOver, setGameOver] = useState(false);
 
   const handleCardClick = (id) => {
+    if (gameOver) {
+      return;
+    }
+
     let newSelectedCards;
 
-    if(selectedCards.includes(id)){
-      if(currentScore > bestScore){
+    if (selectedCards.includes(id)) {
+      if (currentScore > bestScore) {
         setBestScore(currentScore);
       }
 
-      setCurrentScore(0);
+      setGameOver(true);
       newSelectedCards = [];
-
     } else {
       setCurrentScore(currentScore + 1);
       newSelectedCards = [...selectedCards, id];
     }
 
-    if(newSelectedCards.length === pokemonData.length){
+    if (newSelectedCards.length === pokemonData.length) {
       setCurrentScore(0);
       setBestScore(10);
       newSelectedCards = [];
+      setGameOver(true);
     }
 
     setSelectedCards(newSelectedCards);
-  }
+  };
 
+  const handlePlayAgain = () => {
+    setGameOver(false);
+    setCurrentScore(0);
+  };
+
+  const pokemonData = useFetchPokemon(handlePlayAgain);
 
   return (
     <div className='app'>
-      <Score currentScore={currentScore} bestScore={bestScore}/>
-      <Card pokemons={pokemonData} onCardClick={handleCardClick}/>
+      {pokemonData.length === 0 ? (
+        <LoadingScreen />
+      ) : (
+        <>
+          {gameOver && (
+            <GameOver
+              currentScore={currentScore}
+              result={selectedCards.length === pokemonData.length ? 'win' : 'lose'}
+              onPlayAgain={handlePlayAgain}
+            />
+          )}
+          <Score currentScore={currentScore} bestScore={bestScore} />
+          <Card pokemons={pokemonData} onCardClick={handleCardClick} />
+        </>
+      )}
     </div>
-  )
+  );
 }
 
 export default App
